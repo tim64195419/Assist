0481ee0e56250c90570de8de9c58a1158a196e30
tim64195419 <tim64195419@gmail.com>
2015-10-01 00:13:55 +0800

    從eclipse 轉到Android studio
    可以開了試試看吧

---
+++ app/src/main/java/com/example/user/assist/MyFragmentAdapter.java
@@ -0,0 +1,42 @@
+package com.example.user.assist;
+
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MyFragmentAdapter extends FragmentPagerAdapter {
+
+    private List<String> Titles;
+    private List<Integer> Icons;
+
+    public MyFragmentAdapter(FragmentManager fm, List<String> titles, List<Integer> icons) {
+        super(fm);
+        this.Titles = titles;
+        this.Icons = icons;
+    }
+
+    public int getCount() {
+        return 2;
+    }
+
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return Fragment1.newInstance(position);
+            case 1:
+                return Fragment2.newInstance(position);
+            default:
+                return null;
+        }
+    }
+
+    public CharSequence getPageTitle(int position) {
+        return Titles.get(position);
+    }
+
+    public int getPageIcon(int position) {
+        return Icons.get(position);
+    }
+}
